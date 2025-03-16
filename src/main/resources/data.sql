4427981f9970a8eb305b808e05b585628e1001bd
Owidiusz Zieliński <owidiusz@altar.com.pl>
2025-03-16 10:54:24 +0100

    Added init database data when we started application

---
+++ src/main/resources/data.sql
@@ -0,0 +1,60 @@
+--PATIENTS DATA
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Jan', 'Kowalski', 111111111, 8, 0, 1, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Anna', 'Nowak', 222222222, 8, 15, 2, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Piotr', 'Wiśniewski', 333333333, 9, 0, 3, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Katarzyna', 'Lewandowska', 444444444, 9, 30, 4, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Adam', 'Zieliński', 555555555, 10, 0, 5, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Ewa', 'Kowalczyk', 666666666, 10, 30, 6, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Marek', 'Nowicki', 777777777, 11, 0, 7, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Agnieszka', 'Wójcik', 888888888, 11, 15, 8, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Tomasz', 'Kamiński', 999999999, 12, 0, 9, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Monika', 'Krawczyk', 101010101, 12, 30, 10, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Michał', 'Szymański', 111111112, 13, 0, 11, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Olga', 'Dąbrowska', 121212121, 13, 15, 12, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Łukasz', 'Zając', 131313131, 14, 0, 13, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Paulina', 'Jabłońska', 141414141, 14, 30, 14, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Krzysztof', 'Walczak', 151515151, 15, 0, 15, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Dorota', 'Michalska', 161616161, 15, 15, 16, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Artur', 'Piotrowski', 171717171, 16, 0, 17, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Barbara', 'Kwiatkowska', 181818181, 16, 30, 18, false);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Grzegorz', 'Mazur', 191919191, 17, 0, 19, true);
+
+INSERT INTO patient (name, surname, personal_identity_number, visit_hour, visit_minute, ticket_number, accepted)
+VALUES ('Natalia', 'Pawłowska', 202020202, 17, 30, 20, false);
